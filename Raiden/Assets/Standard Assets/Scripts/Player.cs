260ffbc434c575f9e872a7b8c8cc3d74c52d1e17
Nolan Hunt <ninja-sora@hotmail.com>
2014-03-11 19:43:02 -0400

    New Build

---
+++ Raiden/Assets/Standard Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	float moveSpeed = 6.0f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	  //Player controls
+		float hValue = Input.GetAxis ("Horizontal");
+		transform.Translate (new Vector3(hValue*moveSpeed,0,0)*Time.deltaTime);
+		float vValue = Input.GetAxis ("Vertical");
+		transform.Translate (new Vector3(0,0,vValue*moveSpeed)*Time.deltaTime);
+	}
+}
